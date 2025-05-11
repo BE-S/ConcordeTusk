87e9a8679588b8ee3dfc3f9b5a0634f74cd33f15
BE-S <danil.dogi007.danil@mail.ru>
2025-05-11 12:01:56 +0700

    Добавил ограничение количества запросов

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,20 +3,21 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\Route;
+use App\Http\Middleware\EntranceApi;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        commands: __DIR__.'/../routes/console.php',
         using: function () {
-            Route::middleware("api")
+            Route::middleware("throttle:api")
                 ->prefix("api")
                 ->group(base_path("routes/api.php"));
         },
+        commands: __DIR__.'/../routes/console.php',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->group("api", [
-            \App\Http\Middleware\Api::class,
-        ]);
+        $middleware->throttleWithRedis();
+        $middleware->append(EntranceApi::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
